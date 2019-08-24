5a9817ef4d870af164ceaead93bc47f59656df93
mdipierro <massimo.dipierro@gmail.com>
2019-08-24 10:33:14 -0700

    checkpoint

---
+++ example/boards/board_2.py
@@ -0,0 +1,7 @@
+# file: board_2.py
+from kpar import *
+
+# this board has a computer with a different name and fuse
+board = clone(load('boards/board_1.py').board)
+board.name = override('secondary car computer')
+board.fuse.blue.max_power = override(40)
